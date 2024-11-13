c1abc7bb5c534fa608b66da323829cebcab91f84
LombraAtomica <pepeugames2003@gmail.com>
2024-11-13 11:36:46 -0300

    Tela Login

--- src/main/java/com/mycompany/projetofinal/ProjetoFinal.java
+++ src/main/java/com/mycompany/projetofinal/ProjetoFinal.java
@@ -11,6 +11,6 @@ package com.mycompany.projetofinal;
 public class ProjetoFinal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FrameLogin novo = new FrameLogin("Login");
     }
 }
